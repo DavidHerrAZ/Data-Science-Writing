c67136620ac8b2594ad4f47386546ef23f45673b
David Herr <davidherr.dev@gmail.com>
2020-11-08 21:12:48 -0700

    Refactor column cleansing to imported functions.

--- fns_DataWrangling.py
+++ fns_DataWrangling.py
@@ -2,7 +2,35 @@
 import numpy as np
 import pandas as pd
 
-# Pricing must be cleaned as the column contains strings, floats, and ints.
+
+def drop_na_columns(dataframe_list):
+    for dataframe in dataframe_list:
+        dataframe.dropna(axis=1, how='all', inplace=True)
+
+
+def drop_ident_columns(dataframe_list, word_list):
+    for dataframe in dataframe_list:
+        for word in word_list:
+            drop_columns = [
+                col for col in dataframe.columns if col.endswith(word)]
+            dataframe.drop(drop_columns, axis=1, inplace=True)
+
+
+def drop_same_columns(dataframe_list):
+    for dataframe in dataframe_list:
+        nunique = dataframe.apply(pd.Series.nunique)
+        drop_columns = nunique[nunique == 1].index
+        dataframe.drop(drop_columns, axis=1, inplace=True)
+
+
+def drop_nonoverlap_columns(df1, df2):
+    # Find overlapping/instersecting columns between the two datasets
+    intersecting_columns = set(df1.columns) & set(df2.columns)
+
+    # Assign each dataset it's own drop columns based off the complete list of intersections
+    for dataframe in df1, df2:
+        drop_columns = set(dataframe.columns) - intersecting_columns
+        dataframe.drop(drop_columns, axis=1, inplace=True)
 
 
 def clean_currency(x):
